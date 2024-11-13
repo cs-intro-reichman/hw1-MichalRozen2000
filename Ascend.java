db56a85bba436afd6b0443aacc5a136e4878441c
Michal Rozen <michalrozen@Michals-MacBook-Pro.local>
2024-11-13 18:54:54 +0200

    Exersice 1, Question 4

--- Ascend.java
+++ Ascend.java
@@ -1,7 +1,22 @@
-// Generates three integer random numbers in a given range,
-// and prints them in increasing order.
 public class Ascend {
-	public static void main(String[] args) {
-		// Replace this comment with your code
-	}
-}
+  // Replace this comment with your code
+		public static void main (String[] args){
+	
+			int limit = Integer.parseInt(args[0]);
+			int a = (int)(Math.random()*limit);
+			int b = (int)(Math.random()*limit);
+			int c = (int)(Math.random()*limit);
+	
+			System.out.println(a + " " +b+  " " +c);
+	
+			int minimum = Math.min(Math.min(a, b), c);
+			int maximum = Math.max(Math.max(a,b),c);
+			//middle the whole number minus the minimum and the maximum
+			int middle = (limit-(minimum+maximum));
+	
+			System.out.println(minimum + " " + middle + " " + maximum);
+	
+			}
+		  }
+		
+		
